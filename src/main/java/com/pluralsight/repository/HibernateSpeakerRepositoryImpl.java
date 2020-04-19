511ac6da60f7f6cd5ab040249c23ec4fc1ef2995
sewei9 <sebastianweiss2@gmx.de>
2020-04-19 20:07:53 +0200

    Initial commit

---
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    public List<Speaker> findAll() {
+        List<Speaker> speakers = new ArrayList<Speaker>();
+
+        Speaker speaker = new Speaker();
+
+        speaker.setFirstName("Sebastian");
+        speaker.setLastName("Weiss");
+
+        speakers.add(speaker);
+
+        return speakers;
+
+    }
+
+}
